21f0aa055720b0fb42b8a51456c5b4372de71653
4 0 4 \ 2.0 [🇲🇲] <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-07-31 06:30:18 +0630

    native-lib.cpp

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,34 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_myanmar_warpvpn_NativeUtils_getCustomApiUrl(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string customApi = "https://nyeinkokoaung.alwaysdata.net/wg/api.php";
+    return env->NewStringUTF(customApi.c_str());
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_myanmar_warpvpn_NativeUtils_getCfApiBase1(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string cfApi1 = "https://api.cloudflareclient.com/v0i1909051800";
+    return env->NewStringUTF(cfApi1.c_str());
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_myanmar_warpvpn_NativeUtils_getCfApiBase2(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string cfApi2 = "https://api.cloudflareclient.com/v0a2109151800";
+    return env->NewStringUTF(cfApi2.c_str());
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_myanmar_warpvpn_NativeUtils_getCfApiBase3(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string cfApi3 = "https://api.cloudflareclient.com/v0a2409051800";
+    return env->NewStringUTF(cfApi3.c_str());
+}
